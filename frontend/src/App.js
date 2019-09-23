c33c0dfc02b47aac03b696f6267c0874e3c99d25
Robert <robert.pakkanen@helsinki.fi>
2019-09-23 22:58:58 +0300

    created login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,12 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleChange = (event) => {
-    event.preventDefault()
-    console.log(event.target.value)
-    setUsername(event.target.value)
-    setPassword(event.target.value)
-  }
-
   const handleSubmit = async (event) => {
     event.preventDefault()
     const user = await loginService.login({
       username: username,
       password: password
     })
-    
     console.log(user)
   }
 
