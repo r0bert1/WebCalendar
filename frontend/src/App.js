7b56ba548b00b89fba68a7139d05a6f366a85ba0
Robert <robert.pakkanen@helsinki.fi>
2019-09-25 12:26:55 +0300

    refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,72 +4,69 @@ import React, { useState, useEffect } from "react"
 const App = () => {
   const [events, setEvents] = useState([])
 
-  const handleClientLoad = () => {
-    function initClient() {
-      gapi.client.init({
-        apiKey: 'API_KEY',
-        clientId: 'CLIENT_ID',
-        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-        scope: 'https://www.googleapis.com/auth/calendar'
-      }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+  const handleClientLoad = async () => {
+    const initClient = async () => {
+      try {
+        await gapi.client.init({
+          apiKey: 'API_KEY',
+          clientId: 'CLIENT_ID',
+          discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+          scope: 'https://www.googleapis.com/auth/calendar'
+        })
 
-        // Sign in if currently signed out.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
+
         if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
           gapi.auth2.getAuthInstance().signIn()
         }
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      }, function(error) {
-        console.log(error)
-      });
+
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get())
+      } catch (e) {
+        console.log(e)
+      }
     }
 
-    function updateSigninStatus(isSignedIn) {
+    const updateSigninStatus = (isSignedIn) => {
       if (isSignedIn) {
         console.log('you are signed in')
         //listUpcomingEvents()
-        //createCalendar()
+        createCalendar()
       } else {
         console.log('you are signed out')
       }
     }
 
-    const createCalendar = () => {
-      gapi.client.calendar.calendars.insert({
+    const createCalendar = async () => {
+      const response = await gapi.client.calendar.calendars.insert({
         "resource": {"summary": "js client test"}
-      }).then(function(response) {
-        const calendar = response.result
-        console.log(calendar)
       })
+      const calendar = response.result
+      console.log(calendar)
     }
 
-    function listUpcomingEvents() {
-      gapi.client.calendar.events.list({
+    const listUpcomingEvents = async () => {
+      const response = await gapi.client.calendar.events.list({
         'calendarId': 'primary',
         'timeMin': (new Date()).toISOString(),
         'showDeleted': false,
         'singleEvents': true,
         'maxResults': 10,
         'orderBy': 'startTime'
-      }).then(function(response) {
-        var events = response.result.items;
-        console.log('Upcoming events:');
+      })
+      const events = response.result.items
+      console.log('Upcoming events:')
 
-        if (events.length > 0) {
-          for (let i = 0; i < events.length; i++) {
-            var event = events[i];
-            var when = event.start.dateTime;
-            if (!when) {
-              when = event.start.date;
-            }
-            console.log(event.summary + ' (' + when + ')')
+      if (events.length) {
+        events.forEach((event) => {
+          let when = event.start.dateTime
+          if (!when) {
+            when = event.start.date
           }
-        } else {
-          console.log('No upcoming events found.')
-        }
-      });
+          console.log(event.summary + ' (' + when + ')')
+        })
+      } else {
+        console.log('No upcoming events found.')
+      }
     }
 
     gapi.load('client:auth2', initClient)
