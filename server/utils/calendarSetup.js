58271923b866034c4db65e53448fd8329eaa4d79
Robert <robert.pakkanen@helsinki.fi>
2019-11-19 00:35:31 +0200

    cleaning up

--- server/utils/calendarSetup.js
+++ server/utils/calendarSetup.js
@@ -1,38 +1,20 @@
-var { google } = require('googleapis')
-
-//var key = require('../../service_account_key.json')
+const { google } = require('googleapis')
+const { auth } = require('google-auth-library')
 
 const SCOPES = [
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events'
 ]
-/*
-var auth = new google.auth.JWT(
-    key.client_email,
-    null,
-    key.private_key,
-    SCOPES
-)*/
 
-const {auth} = require('google-auth-library');
-
-// load the environment variable with our keys
-const keysEnvVar = process.env['CREDS'];
-//console.log(process.env['CREDS']) 
+const keysEnvVar = process.env['CREDS']
 if (!keysEnvVar) {
-  throw new Error('The $CREDS environment variable was not found!');
+  throw new Error('The $CREDS environment variable was not found!')
 }
-const keys = JSON.parse(keysEnvVar);
+const keys = JSON.parse(keysEnvVar)
 
-
-// load the JWT or UserRefreshClient from the keys
-const client = auth.fromJSON(keys);
-client.scopes = SCOPES;
-
-
+const client = auth.fromJSON(keys)
+client.scopes = SCOPES
 
 const api = google.calendar({version : "v3", auth : client})
-
-
 
 module.exports = api
