947f518e3b01293e546333640622435d008b1e28
Robert <robert.pakkanen@helsinki.fi>
2019-12-02 20:08:44 +0200

    api key is now retrived differently depending on NODE_ENV

--- server/utils/calendarSetup.js
+++ server/utils/calendarSetup.js
@@ -1,19 +1,30 @@
 const { google } = require('googleapis')
 const { auth } = require('google-auth-library')
+const key = require('../../service_account_key.json')
 
 const SCOPES = [
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events'
 ]
 
-const keysEnvVar = process.env['CREDS']
-if (!keysEnvVar) {
-  throw new Error('The $CREDS environment variable was not found!')
-}
-const keys = JSON.parse(keysEnvVar)
+let client
 
-const client = auth.fromJSON(keys)
-client.scopes = SCOPES
+if (process.env.NODE_ENV === 'production') {
+  const keysEnvVar = process.env['CREDS']
+  if (!keysEnvVar) {
+    throw new Error('The $CREDS environment variable was not found!')
+  }
+  const keys = JSON.parse(keysEnvVar)
+  client = auth.fromJSON(keys)
+  client.scopes = SCOPES
+} else {
+  client = new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key,
+    SCOPES
+  )
+}
 
 const api = google.calendar({version : "v3", auth : client})
 
